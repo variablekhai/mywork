c21210da6cec222a7e6d4e70fd60c618d049a3b8
Khai <variablekhai@gmail.com>
2022-03-08 21:34:33 +0800

    + Login/Register functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
-function Home() {
+import React from "react";
+import { useUserAuth } from "../context/UserAuthContext";
 
+function Home() {
+  const { user, logOut } = useUserAuth();
+  console.log(user);
+  const handleLogOut = async () => {
+    try {
+      await logOut();
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
   return (
-      <div>Home Page</div>
+      <div>Home Page { user && user.email} <button onClick={handleLogOut}>Log Out</button></div>
   )
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,45 @@
 import { ThemeProvider } from "@emotion/react";
-import { AppBar, Paper, Container, Grid, TextField, Toolbar, Typography, Button, createMuiTheme, InputAdornment, Link, Divider } from "@mui/material";
+import { Alert, AppBar, Paper, Container, Grid, TextField, Toolbar, Typography, Button, createMuiTheme, InputAdornment, Link, Divider, Icon } from "@mui/material";
 import { createTheme } from "@mui/material";
 import { borderColor, color, textTransform, typography } from "@mui/system";
-import PersonIcon from '@mui/icons-material/Person';
+import EmailIcon from '@mui/icons-material/Email';
 import LockIcon from '@mui/icons-material/Lock';
-import React from "react"
+import React, { useState } from "react"
 import { ReactComponent as Logo } from '../assets/logo.svg'
+import { ReactComponent as GoogleIcon } from '../assets/google.svg'
 import './login.css'
-
+import { useUserAuth } from "../context/UserAuthContext";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const { logIn, googleSignIn } = useUserAuth();
+    const navigate = useNavigate();
+
+    const handleGoogleSignIn = async (e) => {
+        e.preventDefault();
+
+        try {
+            await googleSignIn();
+            navigate("/home");
+        } catch (err) {
+            setError(err.message);
+        }
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+        try {
+            await logIn(email, password);
+            navigate("/home");
+        } catch (err) {
+            setError(err.message);
+        }
+    }
+
     return (
         <div>
             <AppBar
@@ -41,62 +71,86 @@ function Login() {
                     <Grid 
                     item
                     sx={{
-                        my: 2
+                        my: 1
                     }}
                     >
-                        <Typography variant="h4">
+                        <Typography variant="h4" textAlign="center">
                             Login to myWork
                         </Typography>
+                        {error && <Alert severity="error" sx={{ mt: 1, width: 300}}>{ error }</Alert>}
                     </Grid>
                         <Grid item>
-                            <TextField
-                            className="inputField"
-                            placeholder="Username"
-                            size="small"
-                            sx={{
-                                width: 300,
-                            }}
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        <PersonIcon />
-                                    </InputAdornment>
-                                )
-                            }}
-                            />
-                        </Grid>
-                        <Grid item>
-                            <TextField
-                            className="inputField"
-                            size="small"
-                            type="password"
-                            sx={{
-                                width: 300,
-                            }}
-                            placeholder="Password"
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        <LockIcon />
-                                    </InputAdornment>
-                                )
-                            }}
-                            />
+                            <form onSubmit={handleSubmit}>
+                                <TextField
+                                onChange={(e) => {
+                                    setEmail(e.target.value)
+                                }}
+                                className="inputField"
+                                placeholder="Email"
+                                size="small"
+                                sx={{
+                                    width: 300,
+                                    mb: 1
+                                }}
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <EmailIcon />
+                                        </InputAdornment>
+                                    )
+                                }}
+                                /><br/>
+                                <TextField
+                                onChange={(e) => {
+                                    setPassword(e.target.value)
+                                }}
+                                className="inputField"
+                                size="small"
+                                type="password"
+                                sx={{
+                                    width: 300,
+                                    mb: 1
+                                }}
+                                placeholder="Password"
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <LockIcon />
+                                        </InputAdornment>
+                                    )
+                                }}
+                                /><br/>
+                                <Button
+                                type="submit"
+                                variant="contained" 
+                                color="primary"
+                                sx={{
+                                    width: 300,
+                                    py: 1,
+                                    color: '#fff',
+                                    borderRadius: 5
+                                }}
+                                >
+                                Login
+                                </Button>
+                            </form>
                         </Grid>
                         <Grid item>
                             <Button
-                            variant="contained" 
-                            color="primary"
+                            onClick={handleGoogleSignIn}
+                            variant="outlined"
+                            startIcon={<GoogleIcon width={20}/>}
                             sx={{
                                 width: 300,
                                 py: 1,
-                                color: '#fff',
+                                color: '#000',
+                                borderColor: '#000',
                                 borderRadius: 5
                             }}
                             >
-                            Login
+                                Sign in with Google
                             </Button>
-                        </Grid> 
+                        </Grid>
                     </Grid>
                     <Divider
                     sx={{
